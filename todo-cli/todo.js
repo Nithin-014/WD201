35c9daaa9b81b819e2595ca78d378e30bd8afb7d
Nithin-014 <134712982+Nithin-014@users.noreply.github.com>
2023-12-14 19:29:16 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -32,7 +32,7 @@ const todoList = () => {
     list.forEach((item) => {
       if (item.completed || item.dueDate === currentDate) {
         const completedOnSameDate = item.completed && item.dueDate === currentDate;
-        output += `[${completedOnSameDate ? 'X' : ' '}] ${item.title} ${item.dueDate}\n`;
+        output += `[${completedOnSameDate ? 'X' : ' '}] ${item.title} \n`;
       } else if (!item.completed && item.dueDate < currentDate) {
         output += `[ ] ${item.title} ${item.dueDate}\n`;
       } else {
