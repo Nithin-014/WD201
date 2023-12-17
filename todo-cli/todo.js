c146d93c0d503d6073775515fdffb004bd609b9f
Nithin-014 <134712982+Nithin-014@users.noreply.github.com>
2023-12-17 12:08:25 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,46 +1,80 @@
 /* eslint-disable no-undef */
 const todoList = () => {
-  all = []
-  const add = (todoItem) => {
-    all.push(todoItem)
-  }
-  const markAsComplete = (index) => {
-    all[index].completed = true
-  }
+    const all = [];
+    const add = (todoItem) => {
+        all.push(todoItem);
+    };
+    const markAsComplete = (index) => {
+        if (index >= 0 && index < all.length) {
+            all[index].completed = true;
+        } else {
+            throw new Error('Invalid index');
+        }
+    };
 
-  const overdue = () => {
-    return all.filter((item) => item.dueDate < new Date().toLocaleDateString("en-CA"));
-  }
+    const overdue = () => {
+        return all.filter(
+            (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
+        );
+    };
 
-  const dueToday = () => {
-    return all.filter((item)=> item.dueDate === new Date().toLocaleDateString("en-CA"));
-  }
+    const dueToday = () => {
+        return all.filter(
+            (item) => item.dueDate === new Date().toLocaleDateString("en-CA")
+        );
+    };
 
-  const dueLater = () => {
-    return all.filter((item)=> item.dueDate > new Date().toLocaleDateString("en-CA"));
-  }
+    const dueLater = () => {
+        return all.filter(
+            (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
+        );
+    };
 
+    const toDisplayableList = (list) => {
+        const dsl = [];
+        list.forEach((element) => {
+            if (element.dueDate === today) {
+                if (element.completed === true) {
+                    const a = "[x] " + element.title;
+                    dsl.push(a);
+                } else {
+                    const a = "[ ] " + element.title;
+                    dsl.push(a);
+                }
+            } else {
+                if (element.completed === true) {
+                    const a = "[x] " + element.title + " " + element.dueDate;
+                    dsl.push(a);
+                } else {
+                    const a = "[ ] " + element.title + " " + element.dueDate;
+                    dsl.push(a);
+                }
+            }
+        });
+        let g = "";
+        for (let i = 0; i < dsl.length; i++) {
+            // eslint-disable-next-line no-undef
+            obj = dsl[i];
+            if (i === 0) {
+                // eslint-disable-next-line no-undef
+                g = g + obj;
+            } else {
+                // eslint-disable-next-line no-undef
+                g = g + "\n" + obj;
+            }
+        }
+        return g;
+    };
 
-  const toDisplayableList = (list) => {
-    const completionStatus = item.completed ? "[x]" : "";
-    const displayedDate =
-      item.dueDate === new Date().toISOString().slice(0, 10)
-        ? ""
-        : item.dueDate;
-    return list.map(
-      (item) => $(completionStatus) + $(item.title) + $(displayedDate)
-    );
-  };
-
-  return {
-    all,
-    add,
-    markAsComplete,
-    overdue,
-    dueToday,
-    dueLater,
-    toDisplayableList
-  };
+    return {
+        all,
+        add,
+        markAsComplete,
+        overdue,
+        dueToday,
+        dueLater,
+        toDisplayableList,
+    };
 };
 
 module.exports = todoList;
