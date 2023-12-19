3aceeb1cdcab0ff2334a2a3a6ee02b7dc1e7829b
Nithin-014 <134712982+Nithin-014@users.noreply.github.com>
2023-12-19 19:22:46 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -9,52 +9,28 @@ const todoList = () => {
   }
 
   const overdue = () => {
-    const currentDate = new Date();
-    return all.filter((item) => new Date(item.dueDate) < currentDate);
-};
+    return all.filter((item) => item.dueDate < new Date().toLocaleDateString("en-CA"));
+  }
 
-const dueToday = () => {
-  const currentDate = new Date().toISOString().split('T')[0]; 
-  console.log('Current Date:', currentDate);
-  const todayItems = all.filter((item) => {
-      const dueDate = new Date(item.dueDate).toISOString().split('T')[0];
-      return dueDate === currentDate;
-  });
-  console.log('Items Due Today:', todayItems);
-  return todayItems;
-};
+  const dueToday = () => {
+    return all.filter((item)=> item.dueDate === new Date().toLocaleDateString("en-CA"));
+  }
 
-const dueLater = () => {
-  const currentDate = new Date().toISOString().split('T')[0]; 
-  console.log('Current Date:', currentDate);
-  const laterItems = all.filter((item) => {
-      const dueDate = new Date(item.dueDate).toISOString().split('T')[0]; 
-      return new Date(dueDate) > new Date(currentDate);
-  });
-  console.log('Items Due Later:', laterItems);
-  return laterItems;
-};
-
+  const dueLater = () => {
+    return all.filter((item)=> item.dueDate > new Date().toLocaleDateString("en-CA"));
+  }
 
 
   const toDisplayableList = (list) => {
-    const currentDate = new Date().toISOString().split("T")[0];
-    let output = "";
-  
-    list.forEach((item) => {
-      if (item.dueDate === currentDate) {
-        const completedOnSameDate = item.completed && item.dueDate === currentDate;
-        output += `[${completedOnSameDate ? 'x' : ' '}] ${item.title}\n`;
-      } else if (item.dueDate < currentDate) {
-        output += `[ ] ${item.title} ${item.dueDate}\n`;
-      } else {
-        output += `[ ] ${item.title} ${item.dueDate}\n`;
-      }
-    });
-  
-    return output.trim(); 
+    const completionStatus = item.completed ? "[x]" : "";
+    const displayedDate =
+      item.dueDate === new Date().toISOString().slice(0, 10)
+        ? ""
+        : item.dueDate;
+    return list.map(
+      (item) => $(completionStatus) + $(item.title) + $(displayedDate)
+    );
   };
-  
 
   return {
     all,
